2822980a3d86b88dd3d30f33d5d8754f1c9fa4eb
cplant27 <83310529+cplant27@users.noreply.github.com>
2022-09-12 12:38:14 -0700

    bare bones

---
+++ javascript/src/exercises.js
@@ -0,0 +1,15 @@
+function say(word){
+
+    let print = (last + word ?? word)
+
+
+
+
+    console.log (print)
+
+}
+
+function makeCryptoKey(forKey, using, withIV){
+    crypto.createCipheriv(hex, )
+
+}
