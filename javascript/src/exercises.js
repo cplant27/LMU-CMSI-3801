c905602fa78f09c04f5bab3c779b458d1c973dec
cplant27 <83310529+cplant27@users.noreply.github.com>
2022-09-12 17:11:13 -0700

    .

--- javascript/src/exercises.js
+++ javascript/src/exercises.js
@@ -10,6 +10,24 @@ function say(word){
 }
 
 function makeCryptoKey(forKey, using, withIV){
-    crypto.createCipheriv(hex, )
+    crypto.createCipheriv(hex, );
 
+}
+
+function topTenScorers(team){
+    let playersList = Object.entries(team);
+    let leaderboard = [];
+
+    for (; i < playersList.length(); i++){
+        if (totalGames >= 15){
+            if (currentPoints > last.ppg){
+                add to leaderboard
+                sort leaderboard
+            }
+
+        }
+        if (leaderboard.length > 10){
+            leaderboard.length() = 10;
+        }
+    }
 }
