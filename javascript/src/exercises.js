83d50717eca23a0b87907491a8a97a12de8e70b5
Jayden Craig <35790878+jcraig3@users.noreply.github.com>
2022-09-12 22:46:12 -0700

    Update exercises.js

--- javascript/src/exercises.js
+++ javascript/src/exercises.js
@@ -39,3 +39,12 @@ function topTenScorers(team){
         }
     }
 }
+
+async function pokemonInfo(pokemon) {
+  let name = pokemon.target.textContent.toString()
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon/' + name.toLowerCase())
+  const data = await response.json()
+  let pokemonId = data.Id
+  let pokemonName = name
+  let weight = data.weight
+}
