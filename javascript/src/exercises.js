e9fbcf3180f0378f6c333794fb46237c7902c877
cplant27 <83310529+cplant27@users.noreply.github.com>
2022-09-15 01:48:12 -0700

    .

--- javascript/src/exercises.js
+++ javascript/src/exercises.js
@@ -65,28 +65,17 @@ export function* powersGenerator(num, max) {
 //from notes
 
 export function say(word) {
-  if (word === undefined) return phrase;
+  if (word === undefined) return "done.";
   else
-    (phrase) => {
-      if (phrase === undefined) return word;
-      else phrase + word;
+    (say2) => {
+      if (say2 === undefined) return word;
+      else {
+        word = `${word} ${say(word)}`;
+      }
     };
-  say(phrase);
 }
 
-function say2(func) {
-  return function curried(...args) {
-    if (args.length >= func.length) {
-      return func.apply(this, args);
-    } else {
-      return function (...args2) {
-        return curried.apply(this, args.concat(args2));
-      };
-    }
-  };
-}
-
-say2("hello")("how")("are")("you");
+console.log(say("hi")());
 
 export function makeCryptoFunctions(forKey, using, withIV) {
   crypto.createCipheriv(hex);
@@ -116,96 +105,6 @@ export function topTenScorers(inList) {
   return leaderboard;
 }
 
-/*export function topTenScorers(inList) {
-  let teamsList = Object.entries(inList);
-  let leaderboard = [];
-
-  for (const [ind, teamPlayers] in teamsList) {
-    //console.log(`${ind}: ` + teamsList[ind][0]);
-    for (const [key, val] in teamsList[ind][1]) {
-      let stats = teamsList[ind][1][key];
-
-      console.log(stats);
-      if (stats[1] >= 15) {
-        //console.log(`name ${typeof stats[0]}`);
-        //console.log(`ppg: ${stats[2] / stats[1]}`);
-        //console.log(`team: ${typeof teamsList[ind][0]}`);
-        leaderboard += {
-          name: String(stats[0]),
-          ppg: String(stats[2] / stats[1]),
-          team: String(teamsList[ind][0]),
-        };
-        //console.log("leaderboard[-1]" + leaderboard[0]);
-        /*leaderboard.sort(function (a, b) {
-          return a.ppg - b.ppg;
-        });*/
-/*      }
-    }
-    if (leaderboard.length > 10) {
-      leaderboard = leaderboard.slice(0, 10);
-    }
-  }
-  return leaderboard;
-}*/
-
-let input = {
-  ATL: [
-    ["Betnijah Laney", 16, 263],
-    ["Courtney Williams", 14, 193],
-  ],
-  CHI: [
-    ["Kahleah Copper", 17, 267],
-    ["Allie Quigley", 17, 260],
-    ["Courtney Vandersloot", 17, 225],
-  ],
-  CONN: [
-    ["DeWanna Bonner", 16, 285],
-    ["Alyssa Thomas", 16, 241],
-  ],
-  DAL: [
-    ["Arike Ogunbowale", 16, 352],
-    ["Satou Sabally", 12, 153],
-  ],
-  IND: [
-    ["Kelsey Mitchell", 16, 280],
-    ["Tiffany Mitchell", 13, 172],
-    ["Candice Dupree", 16, 202],
-  ],
-  LA: [
-    ["Nneka Ogwumike", 14, 172],
-    ["Chelsea Gray", 16, 224],
-    ["Candace Parker", 16, 211],
-  ],
-  LV: [
-    ["A’ja Wilson", 15, 304],
-    ["Dearica Hamby", 15, 188],
-    ["Angel McCoughtry", 15, 220],
-  ],
-  MIN: [
-    ["Napheesa Collier", 16, 262],
-    ["Crystal Dangerfield", 16, 254],
-  ],
-  NY: [["Layshia Clarendon", 15, 188]],
-  PHX: [
-    ["Diana Taurasi", 13, 236],
-    ["Brittney Griner", 12, 212],
-    ["Skylar Diggins-Smith", 16, 261],
-    ["Bria Hartley", 13, 190],
-  ],
-  SEA: [
-    ["Breanna Stewart", 16, 317],
-    ["Jewell Loyd", 16, 223],
-  ],
-  WSH: [
-    ["Emma Meesseman", 13, 158],
-    ["Ariel Atkins", 15, 212],
-    ["Myisha Hines-Allen", 15, 236],
-  ],
-};
-
-console.log(topTenScorers(input));
-//=======
-
 export async function pokemonInfo(pokemon) {
   let name = pokemon.toString();
   const response = await fetch(
@@ -219,7 +118,6 @@ export async function pokemonInfo(pokemon) {
     weight: data.weight,
   };
 }
-//>>>>>>> 83d50717eca23a0b87907491a8a97a12de8e70b5
 
 export class Quaternion {
   constructor(a, b, c, d) {
@@ -250,10 +148,7 @@ export class Quaternion {
       this.co1 * q.co4 + this.co2 * q.co3 - this.co3 * q.co2 + this.co4 * q.co1;
     return new Quaternion(ans1, ans2, ans3, ans4);
   }
-<<<<<<< HEAD
 
-=======
->>>>>>> 0b75ae2ba6d9221aacf3c84a7ec7c55491e9de9c
   coefficients() {
     let answer = [this.co1, this.co2, this.co3, this.co4];
     return answer;
