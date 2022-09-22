bf36ff7bdec6c3e8ca80546aa2058628f5c95d57
cplant27 <83310529+cplant27@users.noreply.github.com>
2022-09-21 17:03:04 -0700

    .

---
+++ python/exercises.py
@@ -0,0 +1,14 @@
+def change (amt):
+    print('change')
+
+def stretched (word):
+    print('word')
+
+def powers (base, limit):
+    print('powers')
+
+def say(word):
+    print('say')
+
+def find_first_then_lower(input):
+    print("fftl")
