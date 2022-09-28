ea2cbe21ffb599d8b95350e823b539c2707d5f9f
cplant27 <83310529+cplant27@users.noreply.github.com>
2022-09-27 22:59:10 -0700

    .

--- python/exercises.py
+++ python/exercises.py
@@ -1,4 +1,3 @@
-from ctypes.wintypes import WORD
 import math
 import array
 
@@ -9,7 +8,7 @@ def change (amt):
         raise TypeError ("Argument must be of type int.")
 
     if amt < 0:
-        raise ValueError ("Value cannot be negative.")
+        raise ValueError ("amount cannot be negative")
     
     if amt >= 25:
         change[0] = math.floor(amt / 25)
@@ -23,15 +22,16 @@ def change (amt):
         change [2] += 1
         amt -= 5
     
-    change[3] = amt
+    change[3] += amt
 
-    return change
+    return tuple(change)
 
 def stretched (word):
     word = word.replace(" ", "")
+    word = word.replace("\n", "")
+    word = word.replace("\t", "")
     letters_list = list(word)
     index = list(range(1,len(letters_list)+1))
-    #index.pop(0)
     letters_list = list(map(lambda x, y : x * y, letters_list, index))
     return ''.join(letters_list)
 
@@ -45,11 +45,22 @@ def powers (**kwargs):
         pow += 1
 
 
-def say(word):
-  if word == None: return ""
+def say(word = None):
+  if word == None: return ''
   else:
-    return lambda say2: word if say2 == None else say(word + " " + say2)
+    return lambda say2 = None : word if say2 == None else say(word + " " + say2)
 
-print(say("hi")("hi"))
-def find_first_then_lower(input):
-    print("fftl")
+def find_first_then_lower(func, strings):
+    for s in strings:
+        if func(s):
+            return s.lower()
+    raise ValueError
+
+def crypto_functions(input):
+    pass
+
+def top_ten_scorers (input):
+    pass
+
+class Quaternion:
+    pass
