b4181b4cdcc4f293a18f0ef7c1f2f7b0cd6edb1c
Jayden Craig <jayden.t.craig@gmail.com>
2022-11-07 17:06:46 -0800

    Update exercises.swift

--- swift/exercises.swift
+++ swift/exercises.swift
@@ -49,12 +49,12 @@ extension Array {
     }
 }
 
-func powers (of: Int, through: Int) -> Int {
+func powers (of: Int, through: Int, then f: (Int) -> String) -> Int {
     var num = 0
     var power = 0
     while (num <= through) {
-        num = pow(of, power)
-        print(num)
+        num = pow(of, power) as Int
+        f(num)
         power++
     }
 }
